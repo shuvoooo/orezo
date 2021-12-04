a1fca8c92fcd2683001e497d52b514e4acbf5e5f
shuvoooo <shuvo.punam@gmail.com>
2021-12-04 10:27:51 +0600

    home layout init

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// Test purpose
-Route :: get('/', function(){
-    return view('layouts/base');
-});
 
-/*
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-*/
